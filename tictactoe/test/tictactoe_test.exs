96063ad233c3ea1b35b3e062b4ffdcbd3abf2558
Adolfo Neto <adolfo.usp@gmail.com>
2020-08-12 13:54:43 -0300

    Move edited

--- tictactoe/test/tictactoe_test.exs
+++ tictactoe/test/tictactoe_test.exs
@@ -67,6 +67,17 @@ defmodule TictactoeTest do
     assert not Board.winner?(create_board("oxo ooo oxx"), :x)
   end
 
+  test "oox
+        xox
+        ___ is an unfinished board. After a play at 3,3 with x
+        it becomes a finished board with x as winner
+  " do
+    b = Board.new(create_board("oox xox ___"))
+    b = Board.move(b, 3, 3, :x)
+    assert b.status == :finished
+    assert b.winner == :x
+  end
+
   defp assert_equals_board(b1, b2) do
     assert b1 == b2
   end
@@ -76,9 +87,12 @@ defmodule TictactoeTest do
       string
       |> String.replace(" ", "")
       |> String.codepoints()
-      |> Enum.map(fn x -> String.to_atom(x) end)
+      |> Enum.map(fn x -> create_atom_from_string(x) end)
       |> Board.new()
 
     board.cells
   end
+
+  defp create_atom_from_string("_"), do: :empty
+  defp create_atom_from_string(x), do: String.to_atom(x)
 end
